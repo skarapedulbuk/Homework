54e71b20f610cd7c1bfb79d9f8a32b7285577b59
SavitskyS <voronezh2@gmail.com>
2021-08-09 02:01:59 +0300

    Homework02.java обновлен, Задача 2 выполнена.

--- src/Homework01/Homework02.java
+++ src/Homework01/Homework02.java
@@ -3,21 +3,35 @@ package Homework01;
 public class Homework02 {
     public static void main(String[] args) {
         Task1();
+        System.out.println();
+        Task2();
     }
     //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
     // С помощью цикла и условия заменить 0 на 1, 1 на 0;
     public static void Task1 () {
         int[] array = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 0) {
-                array[i] = 1;
-            } else {
-                array[i] = 0;
+        int i = 0;
+        while (i < array.length) {
+            switch (array[i]) {
+                case 0:
+                    array[i] = 1;
+                    break;
+                case 1:
+                    array[i] = 0;
+                    break;
             }
-            System.out.print(array[i]);
+            System.out.print(array[i] + " ");
+            i++;
         }
-        }
+    }
     //2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+    public static void Task2 () {
+        int[] array = new int[8];
+        for (int i = 0; i < 8; i++) {
+            array[i] = i * 3;
+            System.out.print(array[i] + " ");
+        }
+    }
     //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
     //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
     //5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
