05d058a5cfdf203b6ff3bb605fd7192ec2efb0dc
SavitskyS <voronezh2@gmail.com>
2021-08-12 13:53:31 +0300

    Homework03.java обновлен, задача решена.

--- src/Homework01/Homework03.java
+++ src/Homework01/Homework03.java
@@ -9,7 +9,9 @@ public class Homework03 {
     //String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
     // "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
     // "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"}.
-    //При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя, сравнивает его с загаданным словом и сообщает, правильно ли ответил пользователь. Если слово не угадано, компьютер показывает буквы, которые стоят на своих местах.
+    //При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя, сравнивает его
+    // с загаданным словом и сообщает, правильно ли ответил пользователь. Если слово не угадано, компьютер
+    // показывает буквы, которые стоят на своих местах.
     //apple – загаданное
     //apricot - ответ игрока
     //ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
@@ -19,42 +21,53 @@ public class Homework03 {
     //Играем до тех пор, пока игрок не отгадает слово.
     //Используем только маленькие буквы.
     public static void main(String[] args) {
-        playTheGame();
-        /*
-        int [] digits = {1,2};
-        Random random new Random();
-        for (int i = 0; i < digits.length; i++) {
-            int digit = digits[i];
-        }  */
-
-    }
-    public static void playTheGame () {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        playTheGame(words);
+    }
+
+    public static String selectWord (String[] someWords) {
+        boolean isTrue = false;
+        String word = null;
+        while (!isTrue) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Input your word: ");
+            word = sc.nextLine();
+            for (String currentWord:someWords) {
+                if (currentWord.equals(word)) {
+                    isTrue = true;
+                    break;
+                }
+            }
+            if (!isTrue) System.out.println("Unknown word, try again");
+        }
+        return word;
+    }
+    public static void playTheGame (String[] someWords) {
+        System.out.println("Guess the word from the list:");
+        for (String word:someWords) {
+            System.out.print(word + ", ");
+        }
+        System.out.println();
         Random random = new Random();
-        int target = random.nextInt(words.length);
-        System.out.println("Target word: "+ words[target]);
+        int target = random.nextInt(someWords.length);
+    //    System.out.println("Target word: "+ someWords[target]);
         String userWord;
         char[] hiddenWord = new char[15];
         Arrays.fill(hiddenWord, (char) 35);
         do {
-            System.out.print("Hidden word: ");
-            print1DArray(hiddenWord);
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Input your word: ");
-            userWord = sc.nextLine();
-            if (userWord.equals(words[target])) {
+            userWord = selectWord(someWords);
+            if (userWord.equals(someWords[target])) {
                 System.out.println("Congratulations! U win!");
                 break;
             }
-            for (int i = 0; i < words[target].length(); i++) {
-                if (userWord.length() > i) {
-                    if (words[target].charAt(i) == userWord.charAt(i)) {
-                        hiddenWord[i] = userWord.charAt(i);
-                    }
+            for (int i = 0; i < someWords[target].length(); i++) {
+                if (userWord.length() > i && someWords[target].charAt(i) == userWord.charAt(i)) {
+                    hiddenWord[i] = userWord.charAt(i);
                 }
-
             }
-        } while (!userWord.equals(words[target]));
+            System.out.print("Letters that are in place: ");
+            print1DArray(hiddenWord);
+        } while (!userWord.equals(someWords[target]));
         System.out.println("Good bye!");
 
 
