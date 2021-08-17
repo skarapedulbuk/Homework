17bab41e3c94449dfb42f1b6e24e495551f9cce8
SavitskyS <voronezh2@gmail.com>
2021-08-17 10:48:54 +0300

    Homework04.java добавлен Масштаб поля, Проверка диагоналей и линий Незначительные исправления, коммит перед заданием 4

--- src/Homework01/Homework04.java
+++ src/Homework01/Homework04.java
@@ -22,7 +22,6 @@ public class Homework04 {
             fieldSize = scanner.nextInt();
         } while (fieldSize <= 0);
         char[][] field = createField(fieldSize);
-
         do {
             doPlayerMove(field);
             drawField(field);
@@ -34,13 +33,12 @@ public class Homework04 {
                 System.out.println("This is draw.");
                 break;
             }
-
-         //   doAIMove(field);
-         //   drawField(field);
-         //   if (isWin(field, 'O')) {
-         //       System.out.println("Sorry!!! You are looser :(");
-         //       break;
-         //   }
+            doAIMove(field);
+            drawField(field);
+            if (isWin(field, 'O')) {
+                System.out.println("Sorry!!! You are looser :(");
+                break;
+            }
             if (isDraw(field)) {
                 System.out.println("This is draw.");
                 break;
@@ -137,8 +135,8 @@ public class Homework04 {
             return true;
         }
 */
-        boolean isRow = false;
-        int i = 0;
+        boolean isRow;
+        int i;
         for (i = 0;i< field.length;i++) {
             int j = 0;
             do {
@@ -146,13 +144,13 @@ public class Homework04 {
                 isRow = field[i][j] == symbol;
                 j++;
                 } while (isRow && j < field.length);
-            System.out.println("Row " + i + " is "+ isRow);
+            // System.out.println("Row " + i + " is "+ isRow);
             if (isRow) return true;
         }
 
         // vertical
-        boolean isCol = false;
-        i = 0;
+        boolean isCol;
+ //       i = 0;
         for (i = 0;i< field.length;i++) {
             int j = 0;
             do {
@@ -160,28 +158,28 @@ public class Homework04 {
                 isCol = field[j][i] == symbol;
                 j++;
             } while (isCol && j < field.length);
-            System.out.println("Col " + i + " is "+ isCol);
+            //System.out.println("Col " + i + " is "+ isCol);
             if (isCol) return true;
 
         }
 
         // diagonals
-        boolean isDiag = false;
+        boolean isDiag;
         i = 0;
         do {
             isDiag = field[i][i] == symbol;
             i++;
         } while (isDiag && i < field.length);
-        System.out.println("Diag 1 is " + isDiag);
+        //System.out.println("Diag 1 is " + isDiag);
         if (isDiag) return true;
 
-        boolean isDiag2 = false;
+        boolean isDiag2;
         i = 0;
         do {
             isDiag2 = field[i][field.length - i - 1] == symbol;
             i++;
         } while (isDiag2 && i < field.length);
-        System.out.println("Diag 2 is " + isDiag2);
+        //System.out.println("Diag 2 is " + isDiag2);
         if (isDiag2) return true;
 
         return false;
